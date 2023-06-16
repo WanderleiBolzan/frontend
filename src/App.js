9797c6aee7a39e64fff17821a1ed34f2236eba6d
wanderlei jose bolzan <wbolzanjp@gmail.com>
2023-06-16 11:02:00 -0300

    novas entidades

--- src/App.js
+++ src/App.js
@@ -21,20 +21,150 @@ function App() {
     resultados:0.00
   }
 
+  const users = {
+	  id: 0,
+	  nome:"",
+	  email:"",
+	  telefone:"",
+	  perfil_Investidor:"",
+	  senha:""
+  }
+
+  const empresas = {
+    id: 0,
+	  id_Segmento: 0,
+	  razao_Social:"",
+	  acao:""  
+  }
+
+  const segmentos = {
+    id:0,
+	  segmento:""
+  }
+
+  const carteira ={
+  	id:0,
+	  dataCompra:"",
+	  dataVenda:"",
+  	perfil:0
+  }
+
+  const carteiraItems = {
+		id:0,
+  	ativoCarteira:"",
+		nome_ativo:"",
+	  //client: User,
+		valor_contacao: 0.00,
+	  qtd_acoes: 0,
+	  total_investido: 0.00,
+	  rentabilidade: 0.00,
+	  risco: 0.00,
+	  liquidez: 0.00,
+	  volatilidade: 0.00   
+  }
+
+  const mairca = {
+	  id:0,
+	  ativo:"",
+	  data:"",
+	  Id_Segmento:0,
+	  Id_Empresa:0,
+	  Abertura:0.00,
+	  Maxima:0.00,
+	  Minima:0.00,
+	  Fechamento:0.00,
+	  DataCompra:0.00,
+	  Cotacao:0.00,
+	  QtdAcoes:0.00,
+	  TotInvest:0.00,
+	  CotacaoAtual:0.00,
+	  ValorAtual:0.00,
+	  Rentabilidade:0.00,
+	  Risco:0.00,
+	  Liquidez:0.00,
+	  Volatilidade:0.00
+  }
+
+  // - Ativos/Ações---------------------------------------------------------------
   const [btnCadastrar, setbtnCadastrar] = useState(true);
   const [ativos, setAtivos] = useState([]);
   const [objAtivos, setObjAtivos] = useState(ativo);
-
   //UseEfect - Ativos
+
   useEffect(()=>{
     fetch("http://localhost:8080/ativos")
     .then(retorno => retorno.json())
     .then(retorno_convertido => setAtivos(retorno_convertido));
   }, []);
 
-  // Use Effect - Acoes
-  // Use Effect - ativosEmpresasa
+  // Use Effect - Usuários--------------------------------------------------------
+  const [btnCadastrarUser, setbtnCadastrarUser] = useState(true);
+  const [user, setUsers] = useState([]);
+  const [objUser, setObjUsers] = useState(user);
+
+  useEffect(()=>{
+    fetch("http://localhost:8080/users")
+    .then(retorno => retorno.json())
+    .then(retorno_convertido => setAtivos(retorno_convertido));
+  }, []);
+
+  // Use Effect - empresas---------------------------------------------------------
+
+  const [btnCadastrarEmpresas, setbtnCadastrarEmpresas] = useState(true);
+  const [empresa, setEmpresas] = useState([]);
+  const [objEmpresa, setObjEmpresas] = useState(empresa);
+
+  useEffect(()=>{
+    fetch("http://localhost:8080/empresas")
+    .then(retorno => retorno.json())
+    .then(retorno_convertido => setAtivos(retorno_convertido));
+  }, []);
+
+  // Use Effect - Segmentos-------------------------------------------------------
+  const [btnCadastrarSegmentos, setbtnCadastrarSegmentos] = useState(true);
+  const [segmento, setSegmentos] = useState([]);
+  const [objSegmento, setObjSegmentos] = useState(segmento);
+
+  useEffect(()=>{
+    fetch("http://localhost:8080/segmentos")
+    .then(retorno => retorno.json())
+    .then(retorno_convertido => setAtivos(retorno_convertido));
+  }, []);
+
+  // Use Effect - Carteiras-------------------------------------------------------
+  const [btnCadastrarCarteiras, setbtnCadastrarCarteiras] = useState(true);
+  const [carteiras, setCarteiras] = useState([]);
+  const [objCarteiras, setObjCarteiras] = useState(carteira);
+
+  useEffect(()=>{
+    fetch("http://localhost:8080/carteiras")
+    .then(retorno => retorno.json())
+    .then(retorno_convertido => setAtivos(retorno_convertido));
+  }, []);
   
+  // Use Effect - Carteiras Items -------------------------------------------------------
+  const [btnCadastrarCarteirasItem, setbtnCadastrarCarteirasItem] = useState(true);
+  const [carteirasItem, setCarteirasItem] = useState([]);
+  const [objCarteirasItems, setObjCarteirasItems] = useState(carteiraItems);
+
+  useEffect(()=>{
+    fetch("http://localhost:8080/items")
+    .then(retorno => retorno.json())
+    .then(retorno_convertido => setAtivos(retorno_convertido));
+  }, []);
+  
+  // Use Effect - Mairca -------------------------------------------------------
+  const [btnCadastrarMairca, setbtnCadastrarMairca] = useState(true);
+  const [maircas, setMairca] = useState([]);
+  const [objMairca, setObjMairca] = useState(maircas);
+
+  useEffect(()=>{
+    fetch("http://localhost:8080/ahp")
+    .then(retorno => retorno.json())
+    .then(retorno_convertido => setAtivos(retorno_convertido));
+  }, []);
+  
+  //------------------------------------------------------------------------------
 
   const obter = (e)=>{
     setObjAtivos({...objAtivos, [e.target.name]:e.target.value})
